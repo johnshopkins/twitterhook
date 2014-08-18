7c66dbee305329b57a1e0a25b835c8588170bd3c
Jen Wachter <jenleighkelly@gmail.com>
2014-08-18 11:25:03 -0400

    Allowed objects in Token and Consumer parameters. Previously, an array was required.

--- src/TwitterHook/OAuth/Consumer.php
+++ src/TwitterHook/OAuth/Consumer.php
@@ -4,25 +4,25 @@ namespace TwitterHook\OAuth;
 
 class Consumer
 {
-	/**
-	 * Consumer key
-	 * @var string
-	 */
-	protected $key;
+  /**
+   * Consumer key
+   * @var string
+   */
+  protected $key;
 
-	/**
-	 * Consumer Secret
-	 * @var string
-	 */
-	protected $secret;
+  /**
+   * Consumer Secret
+   * @var string
+   */
+  protected $secret;
 
-	public function __construct($consumer)
-	{
-		$this->key = $consumer["key"];
-		$this->secret = $consumer["secret"];
-	}
+  public function __construct($consumer)
+  {
+    $this->key = is_object($consumer) ? $consumer->key : $consumer["key"];
+    $this->secret = is_object($consumer) ? $consumer->secret : $consumer["secret"];
+  }
 
-	public function __get($prop) {
-		return $this->$prop;
-	}
+  public function __get($prop) {
+    return $this->$prop;
+  }
 }
--- src/TwitterHook/OAuth/Token.php
+++ src/TwitterHook/OAuth/Token.php
@@ -4,25 +4,25 @@ namespace TwitterHook\OAuth;
 
 class Token
 {
-	/**
-	 * Token
-	 * @var string
-	 */
-	protected $token;
+  /**
+   * Token
+   * @var string
+   */
+  protected $token;
 
-	/**
-	 * Token Secret
-	 * @var string
-	 */
-	protected $secret;
+  /**
+   * Token Secret
+   * @var string
+   */
+  protected $secret;
 
-	public function __construct($accessToken)
-	{
-		$this->token = $accessToken["token"];
-		$this->secret = $accessToken["secret"];
-	}
+  public function __construct($accessToken)
+  {
+    $this->token = is_object($accessToken) ? $accessToken->token : $accessToken["token"];
+    $this->secret = is_object($accessToken) ? $accessToken->secret : $accessToken["secret"];
+  }
 
-	public function __get($prop) {
-		return $this->$prop;
-	}
+  public function __get($prop) {
+    return $this->$prop;
+  }
 }
